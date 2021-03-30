ade01fc000c192ef820039f1388233bb81f81ea2
casulemarc <casulmarc2014@gmail.com>
2021-03-30 03:32:58 -0400

    Property Complete

--- src/Plot.java
+++ src/Plot.java
@@ -1,4 +1,111 @@
-
+/**
+ * This class represents the plot object
+ * 
+ * @author casul
+ *
+ */
 public class Plot {
+
+	private int x;
+	private int y;
+	private int width;
+	private int depth;
+
+	/**
+	 * initiation of the object's values in a constructor
+	 */
+	public Plot() {
+		x = 0; // not necessarily needed to be declared
+		y = 0; // not necessarily needed to be declared
+		width = 1;
+		depth = 1;
+	}
+
+	/**
+	 * copy constructor
+	 * 
+	 * @param x
+	 * @param y
+	 * @param width
+	 * @param depth
+	 */
+	public Plot(int x, int y, int width, int depth) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.depth = depth;
+	}
+
+	/**
+	 * deep copy of the plot object
+	 * 
+	 * @param p
+	 */
+	public Plot(Plot p) {
+		x = p.x;
+		y = p.y;
+		width = p.width;
+		depth = p.depth;
+
+	}
+
+	// METHODS
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public boolean overlaps(Plot p) {
+
+		// make copy of original object to compare with passed object p
+		Plot house = new Plot();
+		
+		boolean location;
+		
+		//should ise methods or direct x, y?
+		if (house.x == p.x || house.y == p.y) {
+			location = true;
+		} else {
+			location = false;
+		}
+		return location;
+	}
+
+	public boolean encompasses(Plot plot) {
+		// add widith to the x and widht position to get edge?
+	}
+
+	public String toString() {
+		String str = "Upper left: (" + x + "," + y + ") Width: " + width + " Depth: " + depth;
+
+		return str;
+	}
 
 }
--- src/Property.java
+++ src/Property.java
@@ -1,4 +1,156 @@
-
+/**
+ * This class represents the property object
+ * 
+ * @author Mark Kasule
+ *
+ */
 public class Property {
 
+	private String propertyName; // name of property
+	private String city; // name of the city
+	private String owner; // name of house owner
+	private double rentAmount; // holds the plot object
+	private Plot plot;
+
+	/**
+	 * no-arg constructor
+	 */
+	public Property() {
+		propertyName = "";
+		city = "";
+		owner = "";
+		rentAmount = 0;
+		this.plot = new Plot(plot); // default plot
+	}
+
+	/**
+	 * constructor for the property object
+	 * 
+	 * @param propertyName
+	 * @param city
+	 * @param rentAmount
+	 * @param owner
+	 * @param plot
+	 */
+	public Property(String propertyName, String city, double rentAmount, String owner, int x, int y, int width, int depth) {
+		this.propertyName = propertyName;
+		this.city = city;
+		this.owner = owner;
+		this.rentAmount = rentAmount;
+		this.plot = new Plot(x, y, width, depth); // deep copy
+	}
+
+	/**
+	 * deep copy of property object
+	 * 
+	 * @param p
+	 */
+	public Property(Property p) {
+		this.propertyName = p.propertyName;
+		this.city = p.city;
+		this.owner = p.owner;
+		this.rentAmount = p.rentAmount;
+		this.plot = p.plot;
+	}
+
+	/**
+	 * copy constructor without plot object
+	 * 
+	 * @param propertyName
+	 * @param city
+	 * @param rentAmount
+	 * @param owner
+	 */
+	public Property(String propertyName, String city, double rentAmount, String owner) {
+		this.propertyName = propertyName;
+		this.city = city;
+		this.owner = owner;
+		this.rentAmount = rentAmount;
+	}
+
+	// METHODS
+	/**
+	 * sets the name of the property
+	 * 
+	 * @param propertyName
+	 */
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
+	}
+
+	/**
+	 * sets names of the city
+	 * 
+	 * @param city
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * sets owner names
+	 * 
+	 * @param owner
+	 */
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	/**
+	 * sets the rent amount
+	 * 
+	 * @param rentAmount
+	 */
+	public void setRentAmount(double rentAmount) {
+		this.rentAmount = rentAmount;
+	}
+
+	public void setPlot(int x, int y, int width, int depth) {
+		this.plot = new Plot(x, y, width, depth);
+	}
+
+	/**
+	 * method outputs name of property
+	 * 
+	 * @return propertyName
+	 */
+	public String getPropertyName() {
+		return propertyName;
+	}
+
+	/**
+	 * method outputs city name
+	 * 
+	 * @return city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * methods outputs owner names
+	 * 
+	 * @return owner
+	 */
+	public String getOwner() {
+		return owner;
+	}
+
+	/**
+	 * methods outputs rent amount
+	 * 
+	 * @return rent amount
+	 */
+	public double getRentAmount() {
+		return rentAmount;
+	}
+
+	/**
+	 * @return str string to display property details.
+	 */
+	public String toString() {
+		String str = "Property Name: " + propertyName + "\n" + "Located in: " + city + "\n" + "Belonging to: " + owner
+				+ "\n" + "Rent Amount: " + rentAmount + " ";
+		return str;
+	}
 }
